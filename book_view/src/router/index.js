8b26e9adcde5a4801bc14c60a74f24c536e5ad8d
cloudoo <8412893@qq.com>
2019-10-27 22:43:41 +0800

    some route test

--- book_view/src/router/index.js
+++ book_view/src/router/index.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ShowText from '@/components/ShowText'
 
 Vue.use(Router)
+
+const routes = [{
+  path: '/',
+  component: resolv => require(['../pages/index']),
+  meta: { title: 'home' }
+}, {
+  path: '/movieList',
+  component: MoveList
+}, {
+  path: '/showText',
+  component: ShowText
+}]
 
 export default new Router({
   routes: [
